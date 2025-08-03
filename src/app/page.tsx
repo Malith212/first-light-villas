35697c5a8bfa1e56e7bbce36749ae9c8b0d666bc
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-08-03 21:26:33 +0530

    Database Configuration and User Handling part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
+import { connectMongoDB } from "<pages>/config/db";
+import { GetCurrentUserFromMongoDB } from "<pages>/server-actions /users";
 import { UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
+import { UserType } from "<pages>/interfaces";
+connectMongoDB();
 
 export default async function Home() {
+  await GetCurrentUserFromMongoDB();
+
+  const response: any = await GetCurrentUserFromMongoDB();
+  let mongoUser: UserType | null = null;
+  if (response.success) {
+    mongoUser = response.data;
+  }
 
   let clerkUserId = "";
   let name = "";
   let email = "";
 
-  const currentUserData = await currentUser();
-
-  if (currentUserData) {
-    clerkUserId = currentUserData.id;
-    name = currentUserData.firstName + " " + currentUserData.lastName;
-    email = currentUserData.emailAddresses[0].emailAddress || "";
-  } 
+  if (mongoUser) {
+    clerkUserId = mongoUser.clerkUserId;
+    name = mongoUser.name;
+    email = mongoUser.email;
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-5">
-      <h1 className="text-4xl text-gray-500 font-bold uppercase ">First-Light</h1>
+      <h1 className="text-4xl text-gray-500 font-bold uppercase ">HOME PAGE</h1>
       <h1>Clerk User Id : {clerkUserId}</h1>
       <h1>Name : {name}</h1>
       <h1>Email : {email}</h1>
@@ -25,4 +33,3 @@ export default async function Home() {
     </div>
   );
 }
- 
