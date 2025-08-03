ff5e877ded9e32aaf11f1993704c49fb83cd725d
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-08-03 19:31:09 +0530

    sign out added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "antd";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen gap-5">
       <h1 className="text-4xl text-gray-500 font-bold uppercase">First-Light</h1>
-      <Button type="primary">Primary</Button>
-      <Button type="link">Link</Button>
-      <Button type="text">Text</Button>
+      <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
