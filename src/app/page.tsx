12b048497b05f429c301e3188bf3907ed2d9756c
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-08-03 17:42:20 +0530

    antd added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "antd";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl text-gray-500">First-Light</h1>
+    <div className="flex items-center justify-center h-screen gap-5">
+      <h1 className="text-4xl text-gray-500 font-bold uppercase">First-Light</h1>
+      <Button type="primary">Primary</Button>
+      <Button type="link">Link</Button>
+      <Button type="text">Text</Button>
     </div>
   );
 }
